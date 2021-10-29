00130820ffcec8c071043bc76e14f2ba3bbb967b
Artem Ermolayev <erm.art.949@gmail.com>
2021-10-30 00:11:01 +0300

    made it

--- employee.py
+++ employee.py
@@ -17,23 +17,33 @@ class Employee:
         return self._full_name
 
     def get_divided_full_name(self):
-        return self._full_name.split()
+        divided = self._full_name.split()
+        return divided
 
     def get_departments(self):
         return self._departments
 
     def input_full_data(self):
         try:
-            self._full_name = str(input('Введите полное имя сотрудника,\n в формате Фамилия Имя Отчество,'
-                                        ' через пробел и нажмите Enter'))
-            self._address = str(input('Введите адрес сотрудника и нажмите Enter'))
-            self._departments = str(input('Введите названия отделов где числится сотрудник,\n'
-                                          ' через пробел и нажмите Enter')).split()
-            self._birthday = str(input('Введите дату рождения сотрудника,\n'
-                                       ' в формате ДД.ММ.ГГГГ и нажмите Enter'))
-            self._position = str(input('Введите должность сотрудника и нажмите Enter'))
+            self._full_name = input('Введите полное имя сотрудника, в формате Фамилия Имя Отчество,'\
+                                        ' через пробел и нажмите Enter: ')
+            print("full name {}".format(self._full_name))
+            self._address = input('Введите адрес сотрудника и нажмите Enter: ')
+            print("address {}".format(self._address))
+            self._departments = input('Введите названия отделов где числится сотрудник,'\
+                                          ' через пробел и нажмите Enter: ').split()
+            print("departments {}".format(self._departments))
+            self._birthday = input('Введите дату рождения сотрудника,'\
+                                       ' в формате ДД.ММ.ГГГГ и нажмите Enter: ')
+            print("birthday {}".format(self._birthday))
+            self._position = input('Введите должность сотрудника и нажмите Enter: ')
+            print("position {}".format(self._position))
         except:
+            print("EXCEPTION")
             raise Exception()
+
+    def get_full_data(self):
+        return [self._full_name, self._address, self._departments, self._birthday, self._position]
 
     def print_full_data(self):
         print('{} | {} | {} | {} | {}'.format(self._full_name,
